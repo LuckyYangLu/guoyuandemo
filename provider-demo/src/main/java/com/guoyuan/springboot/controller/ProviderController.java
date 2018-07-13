a7b0417ca39f86959f2e0e8071c3c9fbca25b9f2
lu <849802142@qq.com>
2018-07-13 11:03:48 +0800

    first provider hystrix commit

--- provider-demo/src/main/java/com/guoyuan/springboot/controller/ProviderController.java
+++ provider-demo/src/main/java/com/guoyuan/springboot/controller/ProviderController.java
@@ -4,10 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class ProviderController {
 	@RequestMapping(value="/provide",method=RequestMethod.GET)
+	@HystrixCommand(fallbackMethod="processHytrixFallBack")
 	public String getRepsonse(){
 		return "watching video .....";
 	}
+	
+	public String processHytrixFallBack(){
+		return "The service has occured something wrong, please wait!";
+	}
+	
+	
 }
